8ed60077f30a9124677b45f640cfa2738a194423
dstarkey23 <davidstarkey29@gmail.com>
2026-04-05 23:44:28 +0100

    add hybrid model

--- src/echofit/echo_cache.py
+++ src/echofit/echo_cache.py
@@ -3,14 +3,11 @@ from .disk_kernel_cache import precompute_time_differences
 
 
 class EchoCache:
-    def __init__(self, t_model, wavelengths_dict):
+    def __init__(self, t_model, wavelengths):
 
-        self.t_model = t_model
-        self.wavelengths_dict = wavelengths_dict
+        # JAX array (OK)
+        self.t_model = jnp.asarray(t_model)
+        self.dt_matrix = precompute_time_differences(self.t_model)
 
-        # 🔥 COMPUTE ONCE
-        self.dt_matrix = precompute_time_differences(t_model)
-
-        # pre-extract wavelengths list (faster loop)
-        self.bands = list(wavelengths_dict.keys())
-        self.wavelengths = jnp.array(list(wavelengths_dict.values()))
+        # 🔥 FORCE PYTHON DICT (VERY IMPORTANT)
+        self.wavelengths = dict(wavelengths)
